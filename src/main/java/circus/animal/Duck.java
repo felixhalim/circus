64ba88f1c39e6ec5a6202cd011c35042bfd376d1
felixhalim <felixh@comp.nus.edu.sg>
2020-09-11 17:11:51 +0800

    Add constructor

--- src/main/java/circus/animal/Duck.java
+++ src/main/java/circus/animal/Duck.java
@@ -3,12 +3,16 @@ package circus.animal;
 public class Duck extends Bird {
     @Override
     public String speak() {
-        return "Quack Quack";
+        return toString() + "  Quack Quack";
     }
 
     @Override
     public String toString() {
-        return "I'm a Duck";
+        return "I'm " + name + ". I am a Duck!";
+    }
+
+    public void swim() {
+        System.out.println("I'm swimming...");
     }
 
     @Override
@@ -16,7 +20,7 @@ public class Duck extends Bird {
         return 10;
     }
 
-    public void swim() {
-        System.out.println("I'm swimming...");
+    public Duck(String name) {
+        this.name = name;
     }
 }
